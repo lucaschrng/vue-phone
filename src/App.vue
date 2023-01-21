3f102d1e6669c6fddd84414abb5b0517b851bbaf
Lucas Charoing <charoing.lucas@gmail.com>
2023-01-21 18:33:10 +0100

    Fix mobile responsive

--- src/App.vue
+++ src/App.vue
@@ -154,8 +154,8 @@ nav .router-link-active img.contacts-icon--active {
   position: absolute;
   top: 0;
   left: 0;
-  height: 100%;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
